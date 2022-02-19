ed32cb684cfc4e9f25ceb60a50fe0bbf76861e4d
Owilly <pasaclouma54@gmail.com>
2022-02-19 08:47:32 +0300

    add user and credential class

---
+++ user.py
@@ -0,0 +1,12 @@
+
+class User:
+    '''
+ 
+    Args:
+    self.loginUsername: new loginUserName
+    self.password: new password
+    '''
+    
+    def __init__(self, loginUserName, password):
+     self.loginUserName = loginUserName
+     self.password = password
